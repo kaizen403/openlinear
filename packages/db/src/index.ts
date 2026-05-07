ec95ca1fb48ad66bc759f43cb4a8fe1284801ae1
OpenLinear <rishivhavle21@gmail.com>
2026-05-07 11:55:00 +0530

    feat: export crypto module from db package

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,2 +1,8 @@
 export { prisma, default } from "./client";
 export * from "../generated/prisma/client";
+export {
+  encryptToken,
+  decryptToken,
+  isEncryptedToken,
+  tokensEqual,
+} from "./crypto";
